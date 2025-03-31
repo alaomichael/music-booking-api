c0de806ce5c7f78650a144d1a842a70b1c3b4461
Michael Alao <contactleomax@gmail.com>
2025-03-31 23:46:53 +0000

    connect to cloud db

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,18 @@
+// // // const { Pool } = require('pg');
+// // // require('dotenv').config();
+
+// // // const pool = new Pool({
+// // //     host: process.env.DB_HOST,
+// // //     user: process.env.DB_USER,
+// // //     password: process.env.DB_PASSWORD,
+// // //     database: process.env.DB_NAME,
+// // //     port: process.env.DB_PORT,
+// // // });
+
+// // // module.exports = pool;
+
 // // const { Pool } = require('pg');
+// // const fs = require('fs');
 // // require('dotenv').config();
 
 // // const pool = new Pool({
@@ -7,12 +21,18 @@
 // //     password: process.env.DB_PASSWORD,
 // //     database: process.env.DB_NAME,
 // //     port: process.env.DB_PORT,
+// //     ssl: {
+// //         rejectUnauthorized: false, // Set to true if you want to enforce strict validation
+// //         ca: fs.readFileSync('./src/certs/ca.pem').toString(),
+// //         cert: fs.readFileSync('./src/certs/service.cert').toString(),
+// //         key: fs.readFileSync('./src/certs/service.key').toString(),
+// //     },
 // // });
 
 // // module.exports = pool;
 
+
 // const { Pool } = require('pg');
-// const fs = require('fs');
 // require('dotenv').config();
 
 // const pool = new Pool({
@@ -21,18 +41,13 @@
 //     password: process.env.DB_PASSWORD,
 //     database: process.env.DB_NAME,
 //     port: process.env.DB_PORT,
-//     ssl: {
-//         rejectUnauthorized: false, // Set to true if you want to enforce strict validation
-//         ca: fs.readFileSync('./src/certs/ca.pem').toString(),
-//         cert: fs.readFileSync('./src/certs/service.cert').toString(),
-//         key: fs.readFileSync('./src/certs/service.key').toString(),
-//     },
+//     ssl: true, // Enable SSL
 // });
 
 // module.exports = pool;
 
-
 const { Pool } = require('pg');
+const fs = require('fs');
 require('dotenv').config();
 
 const pool = new Pool({
@@ -41,7 +56,10 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    ssl: true, // Enable SSL
+    ssl: {
+        rejectUnauthorized: true, // Enforce strict validation
+        ca: fs.readFileSync('./src/certs/ca.pem').toString(),
+    },
 });
 
 module.exports = pool;
